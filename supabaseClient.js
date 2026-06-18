368691dc781aef8abd08257897a12dbf0ae67453
arena-shahwaj <166806230+arena-shahwaj@users.noreply.github.com>
2026-06-18 16:04:16 +0530

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// 🔑 APNI VALUES YAHAN DAALEIN
-const supabaseUrl = 'https://xahxbhrpsxlrrfyqubud.supabase.co'        // ← Project URL
-const supabaseKey = 'sb_publishable_EadwlJRAR2hzhgXcC3r-Iw_rgp98PUZ'   // ← anon public key
+const supabaseUrl = "https://xahxbhrpsxlrrfyqubud.supabase.co";
+      const supabaseKey = "sb_publishable_EadwlJRAR2hzhgXcC3r-Iw_rgp98PUZ";
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
